aa2499d801614755108bb7a72963c8808c04a128
weizhao33 <91355306+weizhao33@users.noreply.github.com>
2023-12-11 11:40:14 +0800

    Add files via upload

---
+++ model.py
@@ -0,0 +1,210 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import numbers
+
+from einops import rearrange
+
+def to_3d(x):
+    return rearrange(x, 'b c h w -> b (h w) c')
+
+
+def to_4d(x, h, w):
+    return rearrange(x, 'b (h w) c -> b c h w', h=h, w=w)
+
+
+class BiasFree_LayerNorm(nn.Module):
+    def __init__(self, normalized_shape):
+        super(BiasFree_LayerNorm, self).__init__()
+        if isinstance(normalized_shape, numbers.Integral):
+            normalized_shape = (normalized_shape,)
+        normalized_shape = torch.Size(normalized_shape)
+
+        assert len(normalized_shape) == 1
+
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.normalized_shape = normalized_shape
+
+    def forward(self, x):
+        sigma = x.var(-1, keepdim=True, unbiased=False)
+        return x / torch.sqrt(sigma + 1e-5) * self.weight
+
+
+class WithBias_LayerNorm(nn.Module):
+    def __init__(self, normalized_shape):
+        super(WithBias_LayerNorm, self).__init__()
+        if isinstance(normalized_shape, numbers.Integral):
+            normalized_shape = (normalized_shape,)
+        normalized_shape = torch.Size(normalized_shape)
+
+        assert len(normalized_shape) == 1
+
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.normalized_shape = normalized_shape
+
+    def forward(self, x):
+        mu = x.mean(-1, keepdim=True)
+        sigma = x.var(-1, keepdim=True, unbiased=False)
+        return (x - mu) / torch.sqrt(sigma + 1e-5) * self.weight + self.bias
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, dim, LayerNorm_type):
+        super(LayerNorm, self).__init__()
+        if LayerNorm_type == 'BiasFree':
+            self.body = BiasFree_LayerNorm(dim)
+        else:
+            self.body = WithBias_LayerNorm(dim)
+
+    def forward(self, x):
+        h, w = x.shape[-2:]
+        return to_4d(self.body(to_3d(x)), h, w)
+
+
+##########################################################################
+## Feed-Forward Network
+class FeedForward(nn.Module):
+    def __init__(self, dim, ffn_expansion_factor, bias):
+        super(FeedForward, self).__init__()
+
+        # 隐藏层特征维度等于输入维度乘以扩张因子
+        hidden_features = int(dim * ffn_expansion_factor)
+        # 1*1 升维
+        self.project_in = nn.Conv2d(dim, hidden_features * 2, kernel_size=1, bias=bias)
+        # 3*3 分组卷积
+        self.dwconv = nn.Conv2d(hidden_features * 2, hidden_features * 2, kernel_size=3, stride=1, padding=1,
+                                groups=hidden_features * 2, bias=bias)
+        # 1*1 降维
+        self.project_out = nn.Conv2d(hidden_features, dim, kernel_size=1, bias=bias)
+
+    def forward(self, x):
+        x = self.project_in(x)
+        x1, x2 = self.dwconv(x).chunk(2, dim=1)  # 第 1 个维度方向切分成 2 块
+        x = F.gelu(x1) * x2  # gelu 相当于 relu+dropout
+        x = self.project_out(x)
+        return x
+
+
+class MDTA(nn.Module):
+    def __init__(self, channels, num_heads):
+        super(MDTA, self).__init__()
+        self.num_heads = num_heads
+        self.temperature = nn.Parameter(torch.ones(1, num_heads, 1, 1))
+
+        self.qkv = nn.Conv2d(channels, channels * 3, kernel_size=1, bias=False)
+        self.qkv_conv = nn.Conv2d(channels * 3, channels * 3, kernel_size=3, padding=1, groups=channels * 3, bias=False)
+        self.project_out = nn.Conv2d(channels, channels, kernel_size=1, bias=False)
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        q, k, v = self.qkv_conv(self.qkv(x)).chunk(3, dim=1)
+
+        q = q.reshape(b, self.num_heads, -1, h * w)
+        k = k.reshape(b, self.num_heads, -1, h * w)
+        v = v.reshape(b, self.num_heads, -1, h * w)
+        q, k = F.normalize(q, dim=-1), F.normalize(k, dim=-1)
+
+        attn = torch.softmax(torch.matmul(q, k.transpose(-2, -1).contiguous()) * self.temperature, dim=-1)
+        out = self.project_out(torch.matmul(attn, v).reshape(b, -1, h, w))
+        return out
+
+
+class GDFN(nn.Module):
+    def __init__(self, channels, expansion_factor):
+        super(GDFN, self).__init__()
+
+        hidden_channels = int(channels * expansion_factor)
+        self.project_in = nn.Conv2d(channels, hidden_channels * 2, kernel_size=1, bias=False)
+        self.conv = nn.Conv2d(hidden_channels * 2, hidden_channels * 2, kernel_size=3, padding=1,
+                              groups=hidden_channels * 2, bias=False)
+        self.project_out = nn.Conv2d(hidden_channels, channels, kernel_size=1, bias=False)
+
+    def forward(self, x):
+        x1, x2 = self.conv(self.project_in(x)).chunk(2, dim=1)
+        x = self.project_out(F.gelu(x1) * x2)
+        return x
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, channels, num_heads, expansion_factor):
+        super(TransformerBlock, self).__init__()
+        self.norm1 = nn.LayerNorm(channels)
+        self.ffn = GDFN(channels, expansion_factor)
+        self.attn = MDTA(channels, num_heads)
+        self.norm2 = nn.LayerNorm(channels)
+        self.ffn = GDFN(channels, expansion_factor)
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        x = x + self.attn(self.norm1(x.reshape(b, c, -1).transpose(-2, -1).contiguous()).transpose(-2, -1)
+                          .contiguous().reshape(b, c, h, w))
+        x = x + self.ffn(self.norm2(x.reshape(b, c, -1).transpose(-2, -1).contiguous()).transpose(-2, -1)
+                         .contiguous().reshape(b, c, h, w))
+        return x
+
+
+class DownSample(nn.Module):
+    def __init__(self, channels):
+        super(DownSample, self).__init__()
+        self.body = nn.Sequential(nn.Conv2d(channels, channels // 2, kernel_size=3, padding=1, bias=False),
+                                  nn.PixelUnshuffle(2))
+
+    def forward(self, x):
+        return self.body(x)
+
+
+class UpSample(nn.Module):
+    def __init__(self, channels):
+        super(UpSample, self).__init__()
+        self.body = nn.Sequential(nn.Conv2d(channels, channels * 2, kernel_size=3, padding=1, bias=False),
+                                  nn.PixelShuffle(2))
+
+    def forward(self, x):
+        return self.body(x)
+
+
+class Restormer(nn.Module):
+    def __init__(self, num_blocks=[4, 6, 6, 8], num_heads=[1, 2, 4, 8], channels=[48, 96, 192, 384], num_refinement=4,
+                 expansion_factor=2.66):
+        super(Restormer, self).__init__()
+
+        self.embed_conv = nn.Conv2d(3, channels[0], kernel_size=3, padding=1, bias=False)
+
+        self.encoders = nn.ModuleList([nn.Sequential(*[TransformerBlock(
+            num_ch, num_ah, expansion_factor) for _ in range(num_tb)]) for num_tb, num_ah, num_ch in
+                                       zip(num_blocks, num_heads, channels)])
+        # the number of down sample or up sample == the number of encoder - 1
+        self.downs = nn.ModuleList([DownSample(num_ch) for num_ch in channels[:-1]])
+        self.ups = nn.ModuleList([UpSample(num_ch) for num_ch in list(reversed(channels))[:-1]])
+        # the number of reduce block == the number of decoder - 1
+        self.reduces = nn.ModuleList([nn.Conv2d(channels[i], channels[i - 1], kernel_size=1, bias=False)
+                                      for i in reversed(range(2, len(channels)))])
+        # the number of decoder == the number of encoder - 1
+        self.decoders = nn.ModuleList([nn.Sequential(*[TransformerBlock(channels[2], num_heads[2], expansion_factor)
+                                                       for _ in range(num_blocks[2])])])
+        self.decoders.append(nn.Sequential(*[TransformerBlock(channels[1], num_heads[1], expansion_factor)
+                                             for _ in range(num_blocks[1])]))
+        # the channel of last one is not change
+        self.decoders.append(nn.Sequential(*[TransformerBlock(channels[1], num_heads[0], expansion_factor)
+                                             for _ in range(num_blocks[0])]))
+
+        self.refinement = nn.Sequential(*[TransformerBlock(channels[1], num_heads[0], expansion_factor)
+                                          for _ in range(num_refinement)])
+        self.output = nn.Conv2d(channels[1], 3, kernel_size=3, padding=1, bias=False)
+
+    def forward(self, x):
+        fo = self.embed_conv(x)
+        out_enc1 = self.encoders[0](fo)
+        out_enc2 = self.encoders[1](self.downs[0](out_enc1))
+        out_enc3 = self.encoders[2](self.downs[1](out_enc2))
+        out_enc4 = self.encoders[3](self.downs[2](out_enc3))
+
+        out_dec3 = self.decoders[0](self.reduces[0](torch.cat([self.ups[0](out_enc4), out_enc3], dim=1)))
+        out_dec2 = self.decoders[1](self.reduces[1](torch.cat([self.ups[1](out_dec3), out_enc2], dim=1)))
+        fd = self.decoders[2](torch.cat([self.ups[2](out_dec2), out_enc1], dim=1))
+        fr = self.refinement(fd)
+        out = self.output(fr) + x
+        return out
+
